fe7fcaee1d5d56c167064e20ed5a00cdbc816d09
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-01-27 23:55:48 +0200

    Create Fish & update the state

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,26 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
-          <Header tagline="Wes Is Cool" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
